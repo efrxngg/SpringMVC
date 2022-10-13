46af0546d0354ce9c37c2fe182dd97f53bdbbaf7
efrxngg <efrenpgc2602@gmail.com>
2022-10-12 20:08:56 -0500

    search impl

--- src/main/java/edu/spring/mvc/project/controllers/VideoJuegoController.java
+++ src/main/java/edu/spring/mvc/project/controllers/VideoJuegoController.java
@@ -1,12 +1,15 @@
 package edu.spring.mvc.project.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.spring.mvc.project.domain.entity.VideoJuego;
 import edu.spring.mvc.project.service.IBaseService;
@@ -23,16 +26,37 @@ public class VideoJuegoController {
         List<VideoJuego> listaVideoJuegos = null;
         try {
             listaVideoJuegos = service.findAllByActive();
+            model.addAttribute("lista_videojuegos", listaVideoJuegos);
+            model.addAttribute("persona", "Efren");
+            // throw new Exception("");
+            return "views/videojuego/home";
         } catch (Exception e) {
+            model.addAttribute("message", e.getMessage());
             e.printStackTrace();
+            return "views/error/404";
         }
+    }
+
+    @GetMapping(value = "/search")
+    public String search(@RequestParam(value = "query", required = false) String titulo, Model model) throws Exception {
+        System.out.println(titulo);
+        var listaVideoJuegos = service.findAllByActive().stream()
+            .filter(vj -> vj.getTitulo().toLowerCase().contains(titulo.toLowerCase()))
+            .collect(Collectors.toList());
+
         model.addAttribute("lista_videojuegos", listaVideoJuegos);
-        model.addAttribute("persona", "Efren");
+        model.addAttribute("persona", titulo);
         return "views/videojuego/home";
     }
 
-    public String formAppend() {
-        
-        return "";
+    @GetMapping(value = "form")
+    public String formView() {
+        return "views/videojuego/form";
+    }
+
+    @PostMapping(value = "add")
+    public String add() {
+
+        return "views/videojuego/home";
     }
 }
